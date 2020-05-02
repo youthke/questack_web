3985003b7c9f984520322d17453e4a83516d0434
youthke <47767005+youthke@users.noreply.github.com>
2020-05-02 18:05:15 +0900

    make action types

---
+++ context/SignIn/actionCreators.ts
@@ -0,0 +1,13 @@
+import * as types from "./actionTypes"
+
+export const throwRequest = () =>{
+    return {type: types.THROW_POST_REQUEST}
+};
+
+export const changeMail = (mail: string)=>{
+    return {type: types.CHANGE_MAIL, payload: mail}
+};
+
+export const changePassword = (password: string) =>{
+    return {type: types.CHANGE_PASSWORD, payload: password}
+};
---
+++ context/SignIn/actionTypes.ts
@@ -0,0 +1,3 @@
+export const THROW_POST_REQUEST = "SIGN_IN_POST_REQUEST" as const;
+export const CHANGE_MAIL = "SIGN_IN_CHANGE_MAIL" as const;
+export const CHANGE_PASSWORD = "SIGN_IN_CHANGE_PASSWORD" as const;
