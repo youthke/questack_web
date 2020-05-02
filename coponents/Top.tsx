6833f5649835fef923e643c3e9a7fe63d9c5d1f2
youthke <47767005+youthke@users.noreply.github.com>
2020-05-02 17:25:19 +0900

    create top

---
+++ coponents/Top.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import {Button, Card, Col, Layout, Row, Typography} from "antd";
+import Link from "next/link";
+
+export const Top = () =>{
+    return (
+        <Layout>
+            <Layout.Content>
+                <div>
+                    <Row type={"flex"} justify={"center"}>
+                        <Col span={20}>
+                            <Card>
+                                <div>
+                                    <Typography.Title level={2}>
+                                        Questackで楽々質問募集
+                                    </Typography.Title>
+                                </div>
+                                <div>
+                                    <Button>
+                                     <Link href={"/questack/signup"}>
+                                         サインアップはこちらから
+                                     </Link>
+                                    </Button>
+                                </div>
+                            </Card>
+                        </Col>
+                    </Row>
+                </div>
+            </Layout.Content>
+        </Layout>
+    )
+};
---
+++ pages/top/index.tsx
@@ -0,0 +1,17 @@
+import {NextPage} from "next";
+import React from "react";
+import {Top} from "../../coponents/Top";
+
+type Props = {}
+
+const TopPage: NextPage<Props> = () => {
+    return(
+        <div>
+            <Top/>
+        </div>
+    )
+};
+
+
+
+export default TopPage;
