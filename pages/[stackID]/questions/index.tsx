5807bf28b2554315dc6455aa078898d257fcff7d
youthke <47767005+youthke@users.noreply.github.com>
2020-05-06 18:00:47 +0900

    make questions pahe wip

---
+++ pages/[stackID]/questions/index.tsx
@@ -0,0 +1,14 @@
+import {NextPage} from "next";
+import React from "react";
+
+type Props = {}
+
+const QuestionsPage: NextPage<Props> = () => {
+    return (
+        <div>
+            questions
+        </div>
+    )
+};
+
+export default QuestionsPage;
