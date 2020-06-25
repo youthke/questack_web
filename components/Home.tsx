7c6fe64f42c193fe4320b45be369a871964a333d
youthke <47767005+youthke@users.noreply.github.com>
2020-06-26 00:22:05 +0900

    clip board to home

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {Col, Layout, Typography, Row, Card, Table, Button} from "antd";
+import CopyToClipboard from 'react-copy-to-clipboard';
 import {HomeContext} from "../context/Home";
 import Router from "next/router";
 
@@ -20,10 +21,20 @@ export const Home = () =>{
             width: 200,
         },
         {
-            title: "id",
+            title: "url",
             dataIndex: "id",
             key: "id",
-            width: 200
+            width: 200,
+            render: (id: string) =>{
+                return (
+                    <div>
+                        {`${process.env.domainName}/questack/${id}/questions`}
+                    <CopyToClipboard text={`${process.env.domainName}/questack/${id}/questions`}>
+                        <Button>copy</Button>
+                    </CopyToClipboard>
+                    </div>
+                )
+            }
         }
     ];
     return(
