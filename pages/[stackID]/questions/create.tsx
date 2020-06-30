85f8af6af3a7c0dfcd16628d4bfb7a4626aa7af5
youthke <47767005+youthke@users.noreply.github.com>
2020-06-30 19:20:53 +0900

    in success page jump

--- pages/[stackID]/questions/create.tsx
+++ pages/[stackID]/questions/create.tsx
@@ -1,15 +1,35 @@
 import React from "react";
-import {NextPage} from "next";
+import {NextPage, NextPageContext} from "next";
 import {WrappedCreate} from "../../../components/questions/Create";
+import {QuestionCreateContext} from "../../../context/questions/Create";
+import Router from "next/router";
 
-type Props = {}
+type Props = {
+    id: string;
+}
 
-const QuestionCreatePage: NextPage<Props> = ()  =>{
+const QuestionCreatePage: NextPage<Props> = (props: Props)  =>{
+    const {setID, state} = React.useContext(QuestionCreateContext);
+    React.useEffect(() => {
+        setID(props.id)
+    },[]);
+    React.useEffect(() => {
+        if(state.success){
+            Router.push(`/questack/${props.id}/questions`)
+        }
+    },[state.success]);
     return(
         <div>
             <WrappedCreate/>
         </div>
     )
+};
+
+QuestionCreatePage.getInitialProps = async (ctx:NextPageContext) => {
+    const {stackID} = ctx.query;
+    return {
+        id: stackID as string
+    };
 };
 
 export default QuestionCreatePage;
